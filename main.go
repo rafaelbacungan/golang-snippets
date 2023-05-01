bedb76cba85ea0bba65364965ad4f439fabb6d14
rafaelbacungan <rbb52775@gmail.com>
2023-05-01 21:51:13 +0800

    quiz app with concurrency - incomplete

--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import "fmt"
+import "GolangSnippets/quiz"
 
 func main() {
-	vals := []int{2, 4, 0, 100, 4, 11, 2602, 36}
-	fmt.Println(FindOutlier(vals))
-}
-
-/*
-
-You are given an array (which will have a length of at least 3, but could be very large) containing integers.
-The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single
-integer N. Write a method that takes the array as an argument and returns this "outlier" N.
-
-[2, 4, 0, 100, 4, 11, 2602, 36]
-Should return: 11 (the only odd number)
-
-[160, 3, 1719, 19, 11, 13, -21]
-Should return: 160 (the only even number)
-
-*/
-
-func FindOutlier(integers []int) int {
-	var odd []int
-	var even []int
-
-	for _, integer := range integers {
-		if integer%2 == 0 {
-			even = append(even, integer)
-		} else {
-			odd = append(odd, integer)
-		}
-	}
-
-	if len(even) > len(odd) {
-		return odd[0]
-	} else {
-		return even[0]
-	}
+	quiz.Quiz()
 }
---
+++ quiz/quiz.go
@@ -0,0 +1,75 @@
+package quiz
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func Quiz() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of"+
+		"'question,answer'")
+	flag.Parse()
+
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV File.")
+	}
+
+	problems := parseLines(lines)
+	fmt.Println(problems)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	// <-timer.C
+
+	correct := 0
+	for i, p := range problems {
+		select {
+		case <-timer.C:
+			fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+			return
+		default:
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			if answer == p.a {
+				fmt.Println("Correct!")
+				correct++
+			}
+		}
+
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
