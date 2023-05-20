068ed8b2cc97800c3573ff03f23d44a572ff308e
rafaelbacungan <rbb52775@gmail.com>
2023-05-20 11:24:32 +0800

    add base code

--- url-shortener/urlShortener.go
+++ url-shortener/urlShortener.go
@@ -1,7 +1,42 @@
 package url_shortener
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func UrlShortener() {
-	fmt.Printf("Hello World")
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+
+	mapHandler := MapHandler(pathToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yaml := `
+	- path: /urlshort
+	  url: https://github.com/gophercises/urlst
+	- path /urlshort-final
+	  url: https://github.com/gophercises/urlshort/tree/solution
+	`
+
+	yamlHandler, err := YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the sver on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
