749752155fcae9720e9606eb3033f616a54d664e
rafaelbacungan <rbb52775@gmail.com>
2023-05-27 13:31:13 +0800

    binaryMultiple.go - ongoing

---
+++ codewarsSolutions/binaryMultiple.go
@@ -0,0 +1,5 @@
+package codewarsSolutions
+
+func BinaryMultiple() {
+	// Create a regex expression that evaluates whether a binary string is divisible by 3
+}
--- main.go
+++ main.go
@@ -5,9 +5,5 @@ import "GolangSnippets/codewarsSolutions"
 func main() {
 	// url_shortener.UrlShortener()
 	// concurrency.ConcurrencySample()
-	testArray := [][]int{
-		{1, 2, 3, 1, 2, 3},
-		{4, 5, 6, 1, 2, 3},
-		{7, 8, 9, 1, 2, 3}}
-	codewarsSolutions.RotatingArray(testArray)
+	codewarsSolutions.BinaryMultiple()
 }
